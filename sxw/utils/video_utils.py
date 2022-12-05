0780ecce8a7a60b699e03586ccc802cebeaf379a
CGotw <1179526148@qq.com>
2022-12-05 15:57:28 +0800

    视频转换帧率工具

---
+++ sxw/utils/video_utils.py
@@ -0,0 +1,43 @@
+import cv2
+import os
+import platform
+import sys
+
+
+def translate_frame_rate(video_url,dist_url,rate=10):
+    """
+    转换视频到目标帧率
+    :param video_url:
+    :param dist_url:
+    :param rate:
+    :return:
+    """
+
+
+    cap = cv2.VideoCapture()
+    cap.open(video_url)
+    isOpened = cap.isOpened
+    out = cv2.VideoWriter(dist_url,cv2.VideoWriter_fourcc(*'mp4v'),rate,(int(cap.get(3)), int(cap.get(4))))
+
+    origin_rate = cap.get(cv2.CAP_PROP_FPS)
+    origin_frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+
+    multi=int(origin_rate/rate)
+    cnt=0
+    print(multi)
+    while isOpened:
+        ret,frame=cap.read()
+        print(ret)
+        if ret:
+            print(cnt)
+            if cnt==0:
+                print("int",cnt)
+                out.write(frame)
+        else:
+            break
+        cnt = (cnt + 1) % multi
+    out.release()
+
+
+translate_frame_rate("C:\\Users\\11795\Videos\\test_.mp4","C:\\Users\\11795\Videos\\temp.mp4",10)
+
